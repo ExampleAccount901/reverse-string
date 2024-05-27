854bb5c947731b16ea786cdb29dbbe79bceb89a9
Andrew <mr.andrey.kozyrev@gmail.com>
2024-05-28 01:57:08 +0300

    solved another way

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,17 +1,11 @@
-import java.util.Scanner;
-
 public class Solution {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String something = scanner.nextLine();
-        while (true) {
-        String result = reverseString(something);
-        System.out.println(result);
-        something = scanner.nextLine();}
-    }
+    public static String reverseString(String str) {
+        String result = "";
 
-    public static String reverseString(String text) {
-        return new StringBuilder(text).reverse().toString();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            result += str.charAt(i);
         }
 
+        return result;
+    }
 }
