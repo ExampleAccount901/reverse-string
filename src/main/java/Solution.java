50cd1da0ea637f4babf90cc66ceb1da139b722bc
Rasko <raskokun2015@gmail.com>
2024-05-28 01:38:22 +0300

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
-        String result = reverseString("something");
+        Scanner scanner = new Scanner(System.in);
+        String something = scanner.nextLine();
+        while (true) {
+        String result = reverseString(something);
         System.out.println(result);
+        something = scanner.nextLine();}
     }
 
+    public static String reverseString(String text) {
+        return new StringBuilder(text).reverse().toString();
+        }
 
-    // TODO: write a method that takes a string and returns reverse of the string
-    //   Example: "something" ---> "gnihtemos"
-    public static String reverseString(String str) {
-
-        return null;
-    }
 }
